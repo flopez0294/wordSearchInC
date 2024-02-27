470a3495ae260f9dd4509fb8061f0aa9873c29e2
Jose A Lopez Jr <flopez0294@gmail.com>
2024-02-27 11:04:27 -0800

    adding already made files

---
+++ wordsearch.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Declarations of the two functions you will implement
+// Feel free to declare any helper functions or global variables
+void printPuzzle(char **arr);
+void searchPuzzle(char **arr, char *word);
+void fuck(char **arr, char* word, int i, int j, int letIdx, int wordSize) {
+    if (i - 1 >= 0 ) {
+        //fuck
+    }
+    if (j - 1 >= fuck) {
+
+    }
+}
+
+int bSize;
+
+// Main function, DO NOT MODIFY
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <puzzle file name>\n", argv[0]);
+        return 2;
+    }
+    int i, j;
+    FILE *fptr;
+
+    // Open file for reading puzzle
+    fptr = fopen(argv[1], "r");
+    if (fptr == NULL)
+    {
+        printf("Cannot Open Puzzle File!\n");
+        return 0;
+    }
+
+    // Read the size of the puzzle block
+    fscanf(fptr, "%d\n", &bSize);
+
+    // Allocate space for the puzzle block and the word to be searched
+    char **block = (char **)malloc(bSize * sizeof(char *));
+    char *word = (char *)malloc(20 * sizeof(char));
+
+    // Read puzzle block into 2D arrays
+    for (i = 0; i < bSize; i++)
+    {
+        *(block + i) = (char *)malloc(bSize * sizeof(char));
+        for (j = 0; j < bSize - 1; ++j)
+        {
+            fscanf(fptr, "%c ", *(block + i) + j);
+        }
+        fscanf(fptr, "%c \n", *(block + i) + j);
+    }
+    fclose(fptr);
+
+    printf("Enter the word to search: ");
+    scanf("%s", word);
+
+    // Print out original puzzle grid
+    printf("\nPrinting puzzle before search:\n");
+    printPuzzle(block);
+    printf("\n");
+
+    // Call searchPuzzle to the word in the puzzle
+    searchPuzzle(block, word);
+
+    for (i = 0; i < bSize; i++)
+    {
+        free(*(block + i));
+    }
+
+    free(block);
+    free(word);
+    return 0;
+}
+
+void printPuzzle(char **arr)
+{
+    // This function will print out the complete puzzle grid (arr).
+    // It must produce the output in the SAME format as the samples
+    // in the instructions.
+    // Your implementation here...
+    int i, j;
+    for (i = 0; i < bSize; i++)
+    {
+        for (j = 0; j < bSize; j++)
+        {
+            printf("%c ", *(*(arr + i) + j));
+        }
+        printf("\n");
+    }
+}
+
+void searchPuzzle(char **arr, char *word)
+{
+    // This function checks if arr contains the search word. If the
+    // word appears in arr, it will print out a message and the path
+    // as shown in the sample runs. If not found, it will print a
+    // different message as shown in the sample runs.
+    // Your implementation here...
+    int wordSize = sizeof(word);
+    int x;
+    for (x = 0; x < wordSize; x++)
+    {
+        if ((int)*(word + x) >= 'a' && (int)*(word + x) <= 'z') {
+            *(word + x) = ((int)*(word + x) - 32);
+        }
+    }
+
+    // make double for loop to inteate throuhgh array then make recursice function to get around the area.
+    int i, j;
+    for (i = 0; i < bSize; i++) {
+        for (j = 0; j < bSize; j++) {
+
+        }
+    }
+}
